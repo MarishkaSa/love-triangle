9533628d45f126ea631c0d3ea787c1ea9053a219
MarishkaSa <borninsea@gmail.com>
2019-03-16 20:18:40 +0300

    feat: Complete task love-triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let count=0;
+  let lovers=0;
+  
+  for(let s=0; s<preferences.length;s++){  
+       let loverOne=preferences[s];     
+       let loverTwo=preferences[loverOne-1];       
+       let loverThree=preferences[loverTwo-1];         
+     loverThree == s+1 ? count ++ : ''; 
+         count==3 ? lovers++ : '';
+         count==3 ? count = 0 : '';     
+            }     
+  return lovers;
+ }
+
